8ca351b35454cabfdf636bd252535508e4a3a86d
David O'Callaghan <david.ocallaghan@xperi.com>
2022-12-14 15:08:36 +0000

    Added methods to add media to different categories

--- media_tables.py
+++ media_tables.py
@@ -5,14 +5,45 @@ class MediaTable(abc.ABC):
 
     @abc.abstractmethod
     def add_record(self, record):
+        """Adds a record to the associated table
+        Parameters:
+            record (tuple): values of record to add
+        """
         pass
     
     @abc.abstractmethod
     def edit_record(self, id, **kwargs):
+        """Edits values of a specified record in the table
+        Parameters:
+            id (int): rowid of the record to be edited
+            **kwargs: new key-value pair(s) for the record being edited
+        """
         pass
     
     @abc.abstractmethod
     def delete_record(self, id):
+        """Deletes a specified record in the table
+        Parameters:
+            id (int): rowid of the record to be deleted
+        """
+        pass
+
+    @abc.abstractmethod
+    def add_to_category(self, id, category_name):
+        """Adds a specified record to a category
+        Parameters:
+            id (int): rowid of the record
+            category_name (str): name of category to add record to
+        """
+        pass
+    
+    @abc.abstractmethod
+    def remove_from_category(self, id, category_name):
+        """Adds a specified record to a category
+        Parameters:
+            id (int): rowid of the record
+            category_name (str): name of category to add record to
+        """
         pass
 
 
@@ -34,16 +65,72 @@ class MoviesTable(MediaTable):
     
     def edit_record(self, id, **kwargs):
         for key in kwargs:
-            db_cursor.execute(f"UPDATE {self.__table_name} SET {key} = '{kwargs[key]}' WHERE rowid = '{id}'")
+            self.__db_cursor.execute(f"UPDATE {self.__table_name} SET {key} = '{kwargs[key]}' WHERE rowid = '{id}'")
+            self.__db_connection.commit()
+        self.__edit_record_in_categories(id, **kwargs)
+    
+    def __edit_record_in_categories(self, id, **kwargs):
+        table_names = self.__get_table_names()
+        for table_name in table_names:
+            if self.__table_name != table_name:
+                for key in kwargs:
+                    self.__db_cursor.execute(f"UPDATE {table_name} SET {key} = '{kwargs[key]}' WHERE orig_id = '{id}'")
+                    self.__db_connection.commit()
+
     
     def delete_record(self, id):
         self.__db_cursor.execute(f"DELETE from {self.__table_name} WHERE rowid = '{id}'")
         self.__db_connection.commit()
     
     def get_all_records(self):
+        tables = {}
         self.__db_cursor.execute(f"SELECT rowid, * FROM {self.__table_name} ORDER BY rowid")
+        self.__db_connection.commit()
         fetched_data = self.__db_cursor.fetchall()
-        return fetched_data
+        tables[self.__table_name] = fetched_data
+        table_names = self.__get_table_names()
+        for table_name in table_names:
+            if self.__table_name != table_name:
+                self.__db_cursor.execute(f"SELECT rowid, * FROM {table_name} ORDER BY rowid")
+                self.__db_connection.commit()
+                fetched_data = self.__db_cursor.fetchall()
+                tables[table_name] = fetched_data
+        return tables
+    
+    def add_to_category(self, id, category_name):
+        # Get record
+        self.__db_cursor.execute(f"SELECT rowid, * FROM {self.__table_name} WHERE rowid = '{id}'")
+        self.__db_connection.commit()
+        record = self.__db_cursor.fetchone()
+        # Add category table if it doesn't exist
+        table_name = f"{self.__table_name}_{category_name}"
+        db_cursor.execute(f"""CREATE TABLE IF NOT EXISTS {table_name} (
+            orig_id int,
+            title text,
+            director text,
+            year integer
+            )""")
+        # Add record to new category (but only if it hasn't already been added)
+        self.__db_cursor.execute(f"SELECT rowid, * FROM {table_name} WHERE orig_id = '{id}'")
+        self.__db_connection.commit()
+        fetched_data = self.__db_cursor.fetchone()
+        if fetched_data is None:
+            self.__db_cursor.execute(f"INSERT INTO {table_name} VALUES (?, ?, ?, ?)", record)
+            self.__db_connection.commit()
+    
+    def remove_from_category(self, id, category_name):
+        table_name = f"{self.__table_name}_{category_name}"
+        # Remove record from category
+        self.__db_cursor.execute(f"DELETE from {table_name} WHERE orig_id = '{id}'")
+        self.__db_connection.commit()
+
+    def __get_table_names(self):
+        db_cursor.execute("""SELECT name FROM sqlite_master WHERE type='table';""")
+        db_connection.commit()
+        data = db_cursor.fetchall()
+        table_names = [x[0] for x in data]
+        table_names = [x for x in table_names if self.__table_name in x]
+        return table_names
 
     
 class GamesTable(MediaTable):
@@ -64,16 +151,72 @@ class GamesTable(MediaTable):
     
     def edit_record(self, id, **kwargs):
         for key in kwargs:
-            db_cursor.execute(f"UPDATE {self.__table_name} SET {key} = '{kwargs[key]}' WHERE rowid = '{id}'")
+            self.__db_cursor.execute(f"UPDATE {self.__table_name} SET {key} = '{kwargs[key]}' WHERE rowid = '{id}'")
+            self.__db_connection.commit()
+        self.__edit_record_in_categories(id, **kwargs)
+    
+    def __edit_record_in_categories(self, id, **kwargs):
+        table_names = self.__get_table_names()
+        for table_name in table_names:
+            if self.__table_name != table_name:
+                for key in kwargs:
+                    self.__db_cursor.execute(f"UPDATE {table_name} SET {key} = '{kwargs[key]}' WHERE orig_id = '{id}'")
+                    self.__db_connection.commit()
+
     
     def delete_record(self, id):
         self.__db_cursor.execute(f"DELETE from {self.__table_name} WHERE rowid = '{id}'")
         self.__db_connection.commit()
     
     def get_all_records(self):
+        tables = {}
         self.__db_cursor.execute(f"SELECT rowid, * FROM {self.__table_name} ORDER BY rowid")
+        self.__db_connection.commit()
         fetched_data = self.__db_cursor.fetchall()
-        return fetched_data
+        tables[self.__table_name] = fetched_data
+        table_names = self.__get_table_names()
+        for table_name in table_names:
+            if self.__table_name != table_name:
+                self.__db_cursor.execute(f"SELECT rowid, * FROM {table_name} ORDER BY rowid")
+                self.__db_connection.commit()
+                fetched_data = self.__db_cursor.fetchall()
+                tables[table_name] = fetched_data
+        return tables
+    
+    def add_to_category(self, id, category_name):
+        # Get record
+        self.__db_cursor.execute(f"SELECT rowid, * FROM {self.__table_name} WHERE rowid = '{id}'")
+        self.__db_connection.commit()
+        record = self.__db_cursor.fetchone()
+        # Add category table if it doesn't exist
+        table_name = f"{self.__table_name}_{category_name}"
+        db_cursor.execute(f"""CREATE TABLE IF NOT EXISTS {table_name} (
+            orig_id int,
+            title text,
+            director text,
+            year integer
+            )""")
+        # Add record to new category (but only if it hasn't already been added)
+        self.__db_cursor.execute(f"SELECT rowid, * FROM {table_name} WHERE orig_id = '{id}'")
+        self.__db_connection.commit()
+        fetched_data = self.__db_cursor.fetchone()
+        if fetched_data is None:
+            self.__db_cursor.execute(f"INSERT INTO {table_name} VALUES (?, ?, ?, ?)", record)
+            self.__db_connection.commit()
+    
+    def remove_from_category(self, id, category_name):
+        table_name = f"{self.__table_name}_{category_name}"
+        # Remove record from category
+        self.__db_cursor.execute(f"DELETE from {table_name} WHERE orig_id = '{id}'")
+        self.__db_connection.commit()
+
+    def __get_table_names(self):
+        db_cursor.execute("""SELECT name FROM sqlite_master WHERE type='table';""")
+        db_connection.commit()
+        data = db_cursor.fetchall()
+        table_names = [x[0] for x in data]
+        table_names = [x for x in table_names if self.__table_name in x]
+        return table_names
 
 
 class MusicTable(MediaTable):
@@ -96,16 +239,79 @@ class MusicTable(MediaTable):
         for key in kwargs:
             self.__db_cursor.execute(f"UPDATE {self.__table_name} SET {key} = '{kwargs[key]}' WHERE rowid = '{id}'")
             self.__db_connection.commit()
+        self.__edit_record_in_categories(id, **kwargs)
+    
+    def __edit_record_in_categories(self, id, **kwargs):
+        table_names = self.__get_table_names()
+        for table_name in table_names:
+            if self.__table_name != table_name:
+                for key in kwargs:
+                    self.__db_cursor.execute(f"UPDATE {table_name} SET {key} = '{kwargs[key]}' WHERE orig_id = '{id}'")
+                    self.__db_connection.commit()
+
     
     def delete_record(self, id):
         self.__db_cursor.execute(f"DELETE from {self.__table_name} WHERE rowid = '{id}'")
         self.__db_connection.commit()
     
     def get_all_records(self):
+        tables = {}
         self.__db_cursor.execute(f"SELECT rowid, * FROM {self.__table_name} ORDER BY rowid")
+        self.__db_connection.commit()
         fetched_data = self.__db_cursor.fetchall()
-        return fetched_data
+        tables[self.__table_name] = fetched_data
+        table_names = self.__get_table_names()
+        for table_name in table_names:
+            if self.__table_name != table_name:
+                self.__db_cursor.execute(f"SELECT rowid, * FROM {table_name} ORDER BY rowid")
+                self.__db_connection.commit()
+                fetched_data = self.__db_cursor.fetchall()
+                tables[table_name] = fetched_data
+        return tables
+    
+    def add_to_category(self, id, category_name):
+        # Get record
+        self.__db_cursor.execute(f"SELECT rowid, * FROM {self.__table_name} WHERE rowid = '{id}'")
+        self.__db_connection.commit()
+        record = self.__db_cursor.fetchone()
+        # Add category table if it doesn't exist
+        table_name = f"{self.__table_name}_{category_name}"
+        db_cursor.execute(f"""CREATE TABLE IF NOT EXISTS {table_name} (
+            orig_id int,
+            title text,
+            director text,
+            year integer
+            )""")
+        # Add record to new category (but only if it hasn't already been added)
+        self.__db_cursor.execute(f"SELECT rowid, * FROM {table_name} WHERE orig_id = '{id}'")
+        self.__db_connection.commit()
+        fetched_data = self.__db_cursor.fetchone()
+        if fetched_data is None:
+            self.__db_cursor.execute(f"INSERT INTO {table_name} VALUES (?, ?, ?, ?)", record)
+            self.__db_connection.commit()
+    
+    def remove_from_category(self, id, category_name):
+        table_name = f"{self.__table_name}_{category_name}"
+        # Remove record from category
+        self.__db_cursor.execute(f"DELETE from {table_name} WHERE orig_id = '{id}'")
+        self.__db_connection.commit()
 
+    def __get_table_names(self):
+        db_cursor.execute("""SELECT name FROM sqlite_master WHERE type='table';""")
+        db_connection.commit()
+        data = db_cursor.fetchall()
+        table_names = [x[0] for x in data]
+        table_names = [x for x in table_names if self.__table_name in x]
+        return table_names
+
+
+def print_tables(table_obj):
+    tables = table_obj.get_all_records()
+    for table in tables:
+        print(table)
+        for record in tables[table]:
+            print("\t", record)
+    print()
 
 if __name__ == "__main__":
     # persistent storage DB
@@ -120,23 +326,20 @@ if __name__ == "__main__":
 
     movie_record = ('The Shawshank Redemption', 'Frank Darabont', 1994)
     movies_table.add_record(movie_record)
-    movies_records = movies_table.get_all_records()
-    for x in movies_records:
-        print(x)
+    print_tables(movies_table)
     
     game_record = ('Red Dead Redemption 2', 'Rockstar Games', 'PlayStation 4')
     games_table.add_record(game_record)
-    games_records = games_table.get_all_records()
-    for x in games_records:
-        print(x)
+    print_tables(games_table)
     
     music_record = ('Smells Like Teen Spirit', 'Nevermind', 'Nirvana')
     music_table.add_record(music_record)
-    music_records = music_table.get_all_records()
-    for x in music_records:
-        print(x)
+    print_tables(music_table)
 
-    music_table.edit_record(1, song="Come as You Are")
-    music_table.delete_record(1)
     
+    movies_table.add_to_category(1, "favourites")
+    print_tables(movies_table)
+    movies_table.edit_record(1, year=2005)
+    print_tables(movies_table)
+
     db_connection.close()
